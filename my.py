67b2e0a338135548b837b466df032ee34144ba4f
Evlashkin <evlashkin@gmail.com>
2023-02-05 12:03:45 +0300

    my.py and some_file.py were changed

--- my.py
+++ my.py
@@ -6,3 +6,8 @@ while a != 1:
 	pass
 new_s = "This is new string"
 print(new_s)
+digit = int(intput("Введите число, которое я загадал"))
+if digit == a + b:
+	print("Поздравляю, ты угадал! Это число: ", a + b)
+else:
+	print("It's wrong!")
