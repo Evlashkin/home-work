a387eae264fb47a8cdde84d0a59c7867867171cb
Evlashkin/Site-Blog.git <evlashkin@gmail.com>
2023-02-09 19:58:15 +0300

    Created new file 4_module.py

---
+++ Python. Part 1/4_module.py
@@ -0,0 +1,65 @@
+# # Unit 4.2 - task 1
+# a = 6
+# b = 2
+# c = 0
+# if b < a:
+#     c = a * b
+# print(c)
+#
+#
+# # Unit 4.2 - task 2
+# bank = int(input("Введите состояние вашего счета: "))
+# course = 75000
+# if bank >= course:
+#     bank -= course
+#     print("Курс успешно приобретен")
+# print("Хорошего дня!")
+#
+#
+# # Unit 4.2 - task 3
+# father_num = 5
+# son_num = int(input("Какое число я загадал? "))
+# if father_num == son_num:
+#     print("Угадал!")
+# if father_num != son_num:
+#     print("Не угадал!")
+# print("Конец игры")
+#
+#
+# # Unit 4.3 - task 1
+# course = 75000
+# bank = int(input("Сколько денег на счету? "))
+# if course > bank:
+#     print("Не хватает денег на счете")
+# else:
+#     bank -= course
+#     print("Курс успешно приобретен")
+# print("Хорошего дня!")
+#
+#
+# # Unit 4.3 - task 2
+# first_num = int(input("Введите первое число: "))
+# second_num = int(input("Введите второе число: "))
+# summa = int(input("Сумма этих чисел: "))
+# if summa == first_num + second_num:
+#     print("Ответ верный!")
+# else:
+#     print("Ответ неверный")
+#
+#
+# # Unit 4.3 - task 3
+# father_num = 5
+# son_num = int(input("Какое число я загадал? "))
+# if father_num == son_num:
+#     print("Угадал!")
+# else:
+#     print("Не угадал!")
+# print("Конец игры!")
+#
+# father_num = 5
+# son_num = int(input("Какое число я загадал? "))
+# if father_num != son_num:
+#     print("Не угадал!")
+# else:
+#     print("Угадал!")
+# print("Конец игры!")
