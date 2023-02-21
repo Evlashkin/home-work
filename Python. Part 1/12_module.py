60a1db817bba0b390d492bb352d26006ced7061b
Evlashkin/Site-Blog.git <evlashkin@gmail.com>
2023-02-21 20:04:58 +0300

    12_module.py changed

--- Python. Part 1/12_module.py
+++ Python. Part 1/12_module.py
@@ -131,31 +131,189 @@
 #
 #
 # # Unit 12.4 - task 3
-import math
+# import math
+#
+#
+# def my_distance(x, y):
+#     distance = math.sqrt(x**2 + y**2)
+#     print("Дистанция от Вас до указанной точки составляет:", distance)
+#
+#
+# def between_distance(x1, y1, x2, y2):
+#     distance = math.sqrt((x2 - x1)**2 + (y2 - y1))
+#     print("Дистанция между указанными точками составляет:", distance)
+#
+#
+# choise = int(input("Что Вы хотите рассчитать?\n0 - Расстояние от Вас до точки\n1 - Расстояние от точки до точки\n"))
+#
+# if choise == 0:
+#     x = int(input("Введите значение x: "))
+#     y = int(input("Введите значение y: "))
+#     my_distance(x, y)
+#
+# elif choise == 1:
+#     x1 = int(input("Введите значение x первой точки: "))
+#     y1 = int(input("Введите значение y первой точки: "))
+#     x2 = int(input("Введите значение x второй точки: "))
+#     y2 = int(input("Введите значение y второй точки: "))
+#     between_distance(x1, y1, x2, y2)
+# else:
+#     print("Недопустимый вариант ответа.")
+#
+#__________________________________________________________________________
+# # Final work - task 1
+# def summ_n(n):
+#     summ = (1 + n) * n / 2
+#     print("Сумма всех чисел от 1 до", n, "равна:", summ)
+#
+#
+# n = int(input("Введите число N: "))
+# summ_n(n)
+#
+#
+# # Final work - task 2
+# def test():
+#     num = int(input("Введите целое число: "))
+#     if num > 0:
+#         positive()
+#     elif num < 0:
+#         negative()
+#     else:
+#         print("Вы ввели 0")
+#
+#
+# def positive():
+#     print("Положительное")
+#
+#
+# def negative():
+#     print("Отрицательное")
+#
+#
+# test()
+#
+#
+# # Final work - task 3
+# def total_sum(n):
+#     summ = 0
+#     for dig in str(n):
+#         summ += int(dig)
+#     print("Сумма всех цифр в числе равна:", summ)
+#     print()
+#
+#
+# def maximum(n):
+#     max_num = 0
+#     for dig in str(n):
+#         if int(dig) > max_num:
+#             max_num = int(dig)
+#     print("Максимальная цифра в данном числе:", max_num)
+#     print()
+#
+#
+# def minimum(n):
+#     min_num = 10
+#     for dig in str(n):
+#         if int(dig) < min_num:
+#             min_num = int(dig)
+#     print("Минимальная цифра в данном числе:", min_num)
+#     print()
+#
+#
+# while True:
+#     num = int(input("Введите целое число: "))
+#     num = abs(num)
+#     print("Какое действие совершить?")
+#     print("1 - Найти сумму всех цифр числа;")
+#     print("2 - Найти максимальную цифру в числе;")
+#     print("3 - Найти минимальную цифру в числе")
+#     act = int(input("\nВведите номер действия: "))
+#     if act == 1:
+#         total_sum(num)
+#     elif act == 2:
+#         maximum(num)
+#     elif act == 3:
+#         minimum(num)
+#     else:
+#         print("Действие с таким номером отсутствует.")
+#
+#
+# # Final work - task 4
+# def revers_num(num):
+#     new_num = ""
+#     for item in str(num):
+#         new_num = item + new_num
+#     print("Число наоборот:", new_num)
+#
+#
+# def revers_num_without0(num):
+#     new_num = ""
+#     for item in str(num):
+#         if item != "0":
+#             new_num = item + new_num
+#     print("Число наоборот:", new_num)
+#
+#
+# num = int(input("Введите число: "))
+# revers_num(num)
+# print()
+# revers_num_without0(num)
+#
+#
+# # Final work - task 5
+# def count_letters(text, k , n):
+#     text = str.upper(text)
+#     print(text)
+#     count_k = 0
+#     count_n = 0
+#     for item in text:
+#         if item == str(k):
+#             count_k += 1
+#         elif item == n:
+#             count_n += 1
+#     print(f"Количество цифр {k}: {count_k}")
+#     print(f"Количество букв {n}: {count_n}")
+#
+#
+# text = input("Введите текст: ")
+# num = int(input("Какую цифру ищем? "))
+# letter = input("Какую букву ищем? ")
+#
+# count_letters(text, num, letter)
+#
+#
+# # Final work - task 6
+# def mynod(num1, num2):
+#     nod = 1
+#     if num1 >= num2:
+#         while True:
+#             remainder = num1 % num2
+#             if remainder == 0:
+#                 nod = num2
+#                 print("Наибольший делитель :", nod)
+#                 break
+#             else:
+#                 num1 = num2
+#                 num2 = remainder
+#     else:
+#         num1, num2 = num2, num1
+#         mynod(num1, num2)
+#
+#
+# num_1 = int(input("Введите первое число: "))
+# num_2 = int(input("Введите второе число: "))
+# mynod(num_1, num_2)
+#
+#
+# # Final work - task 7
+def rock_paper_scissors():
+  # Здесь будет игра «Камень, ножницы, бумага»
 
+def guess_the_number():
+  # Здесь будет игра «Угадай число»
 
-def my_distance(x, y):
-    distance = math.sqrt(x**2 + y**2)
-    print("Дистанция от Вас до указанной точки составляет:", distance)
+def mainMenu():
+  # Здесь главное меню игры
 
-
-def between_distance(x1, y1, x2, y2):
-    distance = math.sqrt((x2 - x1)**2 + (y2 - y1))
-    print("Дистанция между указанными точками составляет:", distance)
-
-
-choise = int(input("Что Вы хотите рассчитать?\n0 - Расстояние от Вас до точки\n1 - Расстояние от точки до точки\n"))
-
-if choise == 0:
-    x = int(input("Введите значение x: "))
-    y = int(input("Введите значение y: "))
-    my_distance(x, y)
-
-elif choise == 1:
-    x1 = int(input("Введите значение x первой точки: "))
-    y1 = int(input("Введите значение y первой точки: "))
-    x2 = int(input("Введите значение x второй точки: "))
-    y2 = int(input("Введите значение y второй точки: "))
-    between_distance(x1, y1, x2, y2)
-else:
-    print("Недопустимый вариант ответа.")
+mainMenu():
+  pass
